c847c459fa79ff3130dddc9459c6704053fcf80d
Haitham Ali <hali@autolead.digital>
2023-08-09 09:32:17 +0100

    cleanup

--- components/UI/Generic/Typgraphy/MDXSerializers/index.js
+++ components/UI/Generic/Typgraphy/MDXSerializers/index.js
@@ -1,28 +1,32 @@
 export const MDXSerializers = {
   h1: ({ children }) => (
     <>
-      <h1 className="mt-2 mb-6 font-bold text-4xl tracking-tight text-zinc-400 dark:text-slate-200">
+      <h1 className="mt-2 mb-6 font-medium
+      text-5xl
+       leading-tighter
+       rtl:leading-tight
+      text-emerald-600 dark:text-emerald-400">
         {children}
       </h1>
     </>
   ),
   h2: ({ children }) => (
     <>
-      <h2 className="mt-2 mb-4 font-bold text-2xl tracking-tight text-zinc-500 dark:text-slate-100">
+      <h2 className="mt-2 mb-4 font-medium text-3xl tracking-tight text-zinc-500 dark:text-white">
         {children}
       </h2>
     </>
   ),
   p: ({ children }) => (
     <>
-      <p className="mt-2 mb-4 text-lg tracking-tight text-zinc-700 dark:text-slate-100">
+      <p className="mt-2 mb-4 text-xl font-light tracking-loose text-zinc-700 dark:text-slate-100">
         {children}
       </p>
     </>
   ),
   li: ({ children }) => (
     <>
-      <li className="mb-4 text-lg tracking-tight text-zinc-700 dark:text-slate-100">
+      <li className="mb-4 text-xl font-light  tracking-tight text-zinc-700 dark:text-slate-100">
         {children}
       </li>
     </>
